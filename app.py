3c35ce0836ada417a59707aaed76556164d67293
rahman3112 <rahman90143@gmail.com>
2024-12-05 11:47:39 +0530

    Add ML app for deployment

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host='0.0.0.0', port=port)
